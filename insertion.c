55f0cf713035ca14f500129084b0e21dc65ad87c
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2022-09-22 19:20:35 +0530

    Add files via upload

--- insertion.c
+++ insertion.c
@@ -1,36 +1,31 @@
-#include <stdio.h>
-struct array{
-    int arr[100];
-    int size;
-    int length;
-};
-void insert(struct array *arr,int x,int index){
-    int j;
-    j=arr->arr[index];
-        for(int i=arr->length-1;i>=index;i--){
-            arr->arr[i+1]=arr->arr[i];
-        }
-    arr->arr[index]=x;
-    arr->length++;
-    for(int i=0;i<arr->length;i++){
-        printf("%d ",arr->arr[i]);
+//implementing insertion sort
+
+#include<stdio.h>
+void insertion(int a[],int n){
+    int i,j;
+    for(j=1;j<n;j++){
+        int key=a[j];
+        i=j-1;
+        while(i>=0 && a[i]>key){
+        a[i+1]=a[i];
+        i--;
+    }
+    a[i+1]=key;
+    }
+
+    for(i=0;i<n;i++){
+        printf("%d ",a[i]);
     }
 }
+
 int main(){
-      struct array arr;
-      int x,index;
-     printf("enter the integer to insert: "); 
-      scanf("%d",&x);
-       printf("enter the array length: ");
-      scanf("%d",&arr.length);
-       printf("enter the array size: ");
-      scanf("%d",&arr.size);
-      printf("enter the index: ");
-      scanf("%d",&index);
-       printf("enter the array: ");
-      for(int i=0;i<arr.length;i++){
-          scanf("%d",&arr.arr[i]);
-      }
-      insert(&arr,x,index);
-return 0;
+    int a[20];
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    insertion(a,n);
 }
