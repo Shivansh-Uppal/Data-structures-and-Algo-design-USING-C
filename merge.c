89ca85abaf0d1da1bb40afeeddf727deaf610423
Shivansh uppal <74812139+Shivansh-Uppal@users.noreply.github.com>
2022-09-22 19:22:25 +0530

    Add files via upload

--- merge.c
+++ merge.c
@@ -1,47 +1,66 @@
 #include <stdio.h>
 #include<stdlib.h>
-struct array{
-     int a[20];
-     int length;
-};
-void merge(struct array *arr1,struct array *arr2){
-      struct array *arr3=(struct array *)malloc(sizeof(struct array));
-      arr3->length=0;
-     int k=0;
-   
-        for(int i=0;i<arr1->length;i++){
-            arr3->a[k]=arr1->a[i];
-            k++;
-            arr3->length++;
-        }
-        for(int i=0;i<arr2->length;i++){
-            arr3->a[k]=arr2->a[i];
-            k++;
-            arr3->length++;
-        }
-    printf("elements of merged array are: ");
-    for(int l=0;l<arr3->length;l++){
-           printf("%d ",arr3->a[l]);
-    }
-    
-}
-int main(){
-      struct array arr1;
-      struct array arr2;
-      
-      printf("ENTER THE LENGTH OF ARR1: ");
-      scanf("%d",&arr1.length);
-      printf("ENTER THE ELEMENTS OF ARR1: ");
-      for(int i=0;i<arr1.length;i++)
-      scanf("%d",&arr1.a[i]);
-      
-      printf("ENTER THE LENGTH OF ARR2: ");
-      scanf("%d",&arr2.length);
-     printf("ENTER THE ELEMENTS OF ARR2: ");
-      for(int i=0;i<arr2.length;i++)
-      scanf("%d",&arr2.a[i]);
 
-      merge(&arr1,&arr2);
-     
+void merge(int *arr,int s,int m,int l){
+    int i=s,j=m+1,k=0;
+
+    int *temp=(int *)malloc((l-s+1)*sizeof(int));
+    while(i<=m && j<=l){
+        if(arr[i]<arr[j]){
+            temp[k]=arr[i];
+            i++;
+        }
+        else{
+            temp[k]=arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i<=m){
+        temp[k]=arr[i];
+        i++;
+        k++;
+    }
+
+    while(j<=l){
+        temp[k]=arr[j];
+        j++;
+        k++;
+    }
+
+    for(int i=s;i<=l;i++){
+        arr[i]=temp[i-s];
+    }
+
+    free(temp);
+
+    for(int i=0;i<=l;i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+void merge_sort(int *arr, int start, int end){
+    if(start<end){
+        int mid=start+(end-start)/2;
+        merge_sort(arr,start,mid);
+        merge_sort(arr,mid+1,end);
+        merge(arr,start,mid,end);
+    }
+}
+
+int main(){
+    int *arr;
+    int n;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&n);
+    arr=(int *)malloc(n*sizeof(int));
+    printf("Enter the elements: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    merge_sort(arr,0,n-1);
 return 0;
 }
